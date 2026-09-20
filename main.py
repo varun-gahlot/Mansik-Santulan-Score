d52b24b1c0fea21ffe9262e0958af08ef4dd4835
Varun Gahlot <itsvarungahlot@gmail.com>
2026-09-20 17:19:55 +0530

    Updated main file

--- main.py
+++ main.py
@@ -74,4 +74,4 @@ def predict(data: StudentData):
     }])
     
     prediction  = model.predict(input_row)[0]
-    return PredictionResponse(predicted_mental_health_score=round(float(predict),2))
+    return PredictionResponse(predicted_mental_health_score=round(float(prediction),2))
